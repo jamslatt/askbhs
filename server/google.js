bfee67fddc1f4b0722f695fcab63df0e8c5c91a4
James Slattery <james@slattery.tech>
2017-06-09 13:35:15 -0700

    Google+ Integration added.

---
+++ server/google.js
@@ -0,0 +1,9 @@
+ServiceConfiguration.configurations.remove({
+    service: "google"
+});
+ServiceConfiguration.configurations.insert({
+    service: "google",
+    clientId: "818917671060-ld7mluukmc6ib3jcbtstb0s0tvchoks5.apps.googleusercontent.com",
+    loginStyle: "popup",
+    secret: "BSogt3iR4kpY-XzFT9PnBIBD"
+});
